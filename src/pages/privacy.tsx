c170e06f63516aa2e20000929ec5554f13371217
fedoseevaeyu <eyufedoseeva@gmail.com>
2023-03-12 02:37:59 +0300

    add agreement and privacy

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,10 @@
+import Layout from "@app/layout";
+import Content from "@app/(landings)/privacy/page";
+
+export default function Page() {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
+  );
+}
